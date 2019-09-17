0d595500bf6cac23aa1441feb5e64a1d9475d411
JeanleeRoy <jeanlee.barreto@utec.edu.pe>
2019-09-16 21:37:15 -0500

    Type traits list

--- traits_list.h
+++ traits_list.h
@@ -1,6 +1,7 @@
 #ifndef TRAITS_LIST_H
 #define TRAITS_LIST_H
 
+#include <iostream>
 #include "node.h"
 
 template <typename Tr>
@@ -14,38 +15,71 @@ class TraitsList {
         Operation cmp;
 
         bool find(T data, Node<T> **&pointer) {
-            // TODO
+            pointer = &head;
+            while (*pointer != nullptr) {
+                if ((*pointer)->data == data)
+                    return true;
+                if (cmp(data, (*pointer)->data))
+                    return false;
+                pointer = &(*pointer)->next;
+            } return false;
         }
               
     public:
         TraitsList() : head(nullptr) {};
              
         bool insert(T data) {
-            // TODO
+            Node<T> **pointer=nullptr, *node = new Node<T>(data);
+            if (!find(data, pointer)) {
+                node->next = (*pointer);
+                *pointer = node;
+                return true;
+            } return false;
         }
              
         bool remove(T data) {
-            // TODO
+            Node<T> **pointer=nullptr, **temp=nullptr;
+            if (find(data, pointer)) {
+                temp = pointer;
+                *pointer = (*pointer)->next;
+                delete temp;
+                return true;
+            } return false;
         }  
 
         bool find(T data) {
-            // TODO
+            Node<T> **pointer;
+            return find(data, pointer);
         }
 
         T operator [] (int index) {
-            // TODO
+            if (index < 0 || index >= this->size())
+                throw out_of_range("Out of range");
+            Node<T>* temp = this->head;
+            for (int i = 0; i < index; ++i)
+                temp = temp->next;
+            return temp->data;
         }
              
         int size() {
-            // TODO
+            int c = 0;
+            Node<T> *temp = this->head;
+            while (temp != nullptr) {
+                temp = temp->next; c++;
+            } return c;
         }
 
         void print() {
-            // TODO
+            if (this->head != nullptr) {
+                Node<T>* temp = this->head;
+                for (int i = 0; i < this->size(); ++i, temp = temp->next){
+                    cout << temp->data << " ";
+                } cout << endl;
+            } else cout << "Empty List!";
         }
 
         ~TraitsList() {
-            // TODO
+            if (this->head!=nullptr) this->head->killSelf();
         }         
 };
 
